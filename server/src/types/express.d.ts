8aef0fe79d912095ce17ca730af9c88aba917c8e
Tejashwini-Git <tejashwinihkotyar06@gmail.com>
2025-11-20 16:21:25 +0530

    Create express.d.ts

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,17 @@
+import { Request } from "express";
+
+declare global {
+  namespace Express {
+    interface UserPayload {
+      id: string;
+      email: string;
+      role?: string;
+    }
+
+    interface Request {
+      user?: UserPayload;
+    }
+  }
+}
+
+export {};
